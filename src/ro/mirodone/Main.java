ce4b3e1a0768c915d49687e08e56702b82774d77
apartment81 <bogdan.mirodone@gmail.com>
2019-05-08 21:17:18 +0300

    Server app - multiple connections

--- src/ro/mirodone/Main.java
+++ src/ro/mirodone/Main.java
@@ -1,11 +1,9 @@
 package ro.mirodone;
 
-import java.io.BufferedReader;
+
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
-import java.net.Socket;
+
 
 public class Main {
 
@@ -13,21 +11,25 @@ public class Main {
 
         try(ServerSocket serverSocket = new ServerSocket(5000)){
 
-            Socket socket = serverSocket.accept();
+           /* Socket socket = serverSocket.accept();
             System.out.println("Client connected");
             BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);*/
 
             //this infinite loop will wait for a response from the Client though method .readLine()
-            while (true){
-                String echoString = input.readLine();
-                if (echoString.equals("exit")){
-                    break;
-                }
-                output.println("Echo from server " + echoString);
-            }
+            //moving all the above* code inside the loop, we can achieve multiple connections
 
+            while (true){
+                //all the code was moved to Echoer.java class
+
+                //we start a new thread for every connection created
+
+                new Echoer(serverSocket.accept()).start();
+//                Socket socket = serverSocket.accept();
+  //              Echoer echoer = new Echoer(socket);
+    //            echoer.start();
+
+            }
         }catch (IOException e) {
             System.out.println("Server exception " + e.getMessage());
         }
